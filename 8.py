ae07cf4a68d11a79a5a89db40a8a65195af8a002
Федор <zinchenkofs@yandex.ru>
2022-12-24 14:57:33 +0300

    git commit

--- 8.py
+++ 8.py
@@ -1,5 +1,13 @@
-for x in '0123456789ABCD':
+'''for x in '0123456789ABCD':
     t = int('1' + x + '563', 14) + int('871' + x + '3', 14)
     if t % 24 == 0:
         print(t // 24)
-        exit()
+        exit()'''
+
+x = 5 ** 36 + 5**24 - 25
+s = ''
+while x != 0:
+    s += str(x % 5)
+    x //= 5
+s = s[::-1]
+print(s.count("4"))
